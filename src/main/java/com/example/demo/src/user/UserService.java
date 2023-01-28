e4e46b0f0c98d753e807bd1a64bca0bbcefbde26
dgw0620 <105223542+dgw0620@users.noreply.github.com>
2023-01-28 22:39:42 +0900

    Update UserService.java

--- src/main/java/com/example/demo/src/user/UserService.java
+++ src/main/java/com/example/demo/src/user/UserService.java
@@ -99,4 +99,22 @@ public class UserService {
             throw new BaseException(DATABASE_ERROR);
         }
     }
+    
+    // 회원정보 수정(Patch)
+    public void modifyUserPassword(PatchUserReq patchUserReq) throws BaseException {
+        if(userProvider.checkEmail(patchUserReq.getUser_email()) == 0) {
+            throw new BaseException(NON_EXISTENT_EMAIL);
+        }
+        String pwd;
+        try {
+            pwd = new SHA256().encrypt(patchUserReq.getUser_password());
+            patchUserReq.setUser_password(pwd);
+            int result = userDao.modifyUserPassword(patchUserReq); // 해당 과정이 무사히 수행되면 True(1), 그렇지 않으면 False(0)입니다.
+            if (result == 0) { // result값이 0이면 과정이 실패한 것이므로 에러 메서지를 보냅니다.
+                throw new BaseException(MODIFY_FAIL_USERPASSWORD);
+            }
+        } catch (Exception exception) { // DB에 이상이 있는 경우 에러 메시지를 보냅니다.
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
 }
