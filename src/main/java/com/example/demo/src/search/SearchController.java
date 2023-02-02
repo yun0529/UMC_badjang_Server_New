edfd9a2e791ae4c50dc0a14b7261ee98626e955f
yuju9 <lah930@naver.com>
2023-02-02 22:48:50 +0900

    response 수정

--- src/main/java/com/example/demo/src/search/SearchController.java
+++ src/main/java/com/example/demo/src/search/SearchController.java
@@ -80,7 +80,7 @@ public class SearchController {
     public BaseResponse<GetSearchAllRes> searchAll(@RequestParam(value="query") String query) {
         try {
             if (query == null || query.equals("")) {
-                return new BaseResponse<>(REQUEST_ERROR);
+                return new BaseResponse<>(GET_SEARCH_EMPTY_QUERY);
             }
             if (query.length() > 50) {
                 return new BaseResponse<>(GET_SEARCH_INVALID_QUERY);
