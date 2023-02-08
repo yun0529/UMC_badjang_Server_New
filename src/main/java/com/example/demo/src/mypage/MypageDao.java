3a8d32e3fcd19ac67a39f337ba91c0ef14459bdf
Yongjin Jeon <yjin1185@gmail.com>
2023-02-08 18:52:38 +0900

    프로필 수정

--- src/main/java/com/example/demo/src/mypage/MypageDao.java
+++ src/main/java/com/example/demo/src/mypage/MypageDao.java
@@ -19,12 +19,28 @@ public class MypageDao {
 
 
     public GetMypageRes getMypage(int user_idx) {
-        String getUserQuery = "select user_name, user_profileimage_url from User where user_idx = ?"; // 해당 user_id를 만족하는 유저를 조회하는 쿼리문
+        String getUserQuery = "select user_idx, user_email, user_name, user_profileimage_url, user_type, user_birth, user_phone\n" +
+                ", user_push_yn, user_on_off, user_univ, user_college, user_department, user_grade, user_semester, user_province,\n" +
+                "  user_city from User where user_idx = ?"; // 해당 user_id를 만족하는 유저를 조회하는 쿼리문
         int getUserParams = user_idx;
         return this.jdbcTemplate.queryForObject(getUserQuery,
                 (rs, rowNum) -> new GetMypageRes(
+                        rs.getInt("user_idx"),
+                        rs.getString("user_email"),
                         rs.getString("user_name"),
-                        rs.getString("user_profileimage_url")), // RowMapper(위의 링크 참조): 원하는 결과값 형태로 받기
+                        rs.getString("user_profileimage_url"),
+                        rs.getString("user_type"),
+                        rs.getString("user_birth"),
+                        rs.getString("user_phone"),
+                        rs.getString("user_push_yn"),
+                        rs.getString("user_on_off"),
+                        rs.getString("user_univ"),
+                        rs.getString("user_college"),
+                        rs.getString("user_department"),
+                        rs.getString("user_grade"),
+                        rs.getString("user_semester"),
+                        rs.getString("user_province"),
+                        rs.getString("user_city")), // RowMapper(위의 링크 참조): 원하는 결과값 형태로 받기
                 getUserParams); // 한 개의 회원정보를 얻기 위한 jdbcTemplate 함수(Query, 객체 매핑 정보, Params)의 결과 반환
     }
 
