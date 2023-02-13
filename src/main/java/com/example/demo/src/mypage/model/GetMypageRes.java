1f5b81fa6bcec913e38948b3a17f07af38ef1246
choyunhee <xornd1110@naver.com>
2023-02-13 16:35:44 +0900

    알림 설정부분 수정

--- src/main/java/com/example/demo/src/mypage/model/GetMypageRes.java
+++ src/main/java/com/example/demo/src/mypage/model/GetMypageRes.java
@@ -8,8 +8,22 @@ import lombok.Setter;
 @Setter // 해당 클래스에 대한 설정자 생성
 @AllArgsConstructor
 public class GetMypageRes {
+    private int user_idx;
+    private String user_email;
     private String user_name;
     private String user_profileimage_url;
+    private String user_type;
+    private String user_birth;
+    private String user_phone;
+    private String user_push_yn;
+    private String user_on_off;
+    private String user_univ;
+    private String user_college;
+    private String user_department;
+    private String user_grade;
+    private String user_semester;
+    private String user_province;
+    private String user_city;
     private String bookmark_yn;
     private String new_post_yn;
     private String inq_answer_yn;
